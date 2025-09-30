dd3b3295431a1e79b961df5fb5d04fc869702aec
Scott Aron Bloom <scott@towel42.com>
2025-09-30 23:46:27 +0100

    Better debugging

--- examples/CLI/main.cpp
+++ examples/CLI/main.cpp
@@ -5,14 +5,56 @@
 #include <QFileInfo>
 
 #include <iostream>
+#include <thread>
+#include <chrono>
+
+void setupDebug( int debugLevel, int port )
+{
+    std::vector< std::pair< QString, bool > > catVector =   //
+        {
+            { "js", false },   //
+            { "Towel42.Qt6MathJax", false },
+            { "Towel42.Qt6MathJax.Console", false },
+            { "Towel42.Qt6MathJax.Debug", false },
+            { "Towel42.Qt6MathJax.QRC", false }   //
+        };
+    if ( debugLevel > 0 )
+    {
+        NTowel42::CQt6MathJax::enableDebugConsole( port );
+        catVector[ 0 ].second = true;
+        catVector[ 1 ].second = true;
+    }
+
+    if ( debugLevel > 1 )
+    {
+        catVector[ 2 ].second = true;
+    }
+
+    if ( debugLevel > 2 )
+    {
+        catVector[ 3 ].second = true;
+    }
+
+    if ( debugLevel > 3 )
+    {
+        catVector[ 4 ].second = true;
+    }
+
+    QStringList categories;
+    for ( auto &&ii : catVector )
+    {
+        categories << QString( "%1=%2" ).arg( ii.first ).arg( ii.second ? "true" : "false" );
+    }
+
+    QLoggingCategory::setFilterRules( categories.join( "\n" ) );
+}
 
 int main( int argc, char *argv[] )
 {
     QApplication appl( argc, argv );
+
     QCoreApplication::setApplicationName( "Qt6MathJax-CLIExample" );
     QCoreApplication::setApplicationVersion( "1.0" );
-
-    QLoggingCategory::setFilterRules( ( QStringList() << "Towel42.Qt6MathJax.*=false" ).join( "\n" ) );
 
     QCommandLineParser parser;
     parser.setApplicationDescription( "Qt6MathJax-CLIExample - A simple tool to convert TeX strings into SVG." );
@@ -24,6 +66,19 @@ int main( int argc, char *argv[] )
 
     auto outputFileOption = QCommandLineOption( QStringList() << "output" << "o", "Output SVG file (svg extension will be added if missing) <default=output.svg>", "output file", "output.svg" );
     parser.addOption( outputFileOption );
+
+    auto debugOption = QCommandLineOption(
+        QStringList() << "debug" << "d",
+        "Debug level <default=0>\n"
+        "1 - Enables the Chromium Debug System at the port set via -port, as well as the 'js', 'Towel42.Qt6MathJax' logging categories\n"
+        "2 - Enables level 1 and the Towel42.Qt6MathJax.Console logging category\n"
+        "3 - Enables level 2 and the Towel42.Qt6MathJax.Debug\n"
+        "4 - Enables level 2 and the Towel42.Qt6MathJax.QRC",
+        "level", "0" );
+    parser.addOption( debugOption );
+
+    auto debugPortOption = QCommandLineOption( QStringList() << "port" << "p", "Debug Port <default=12345>", "port", "12345" );
+    parser.addOption( debugPortOption );
 
     parser.process( appl );
     if ( !parser.unknownOptionNames().isEmpty() )
@@ -58,6 +113,22 @@ int main( int argc, char *argv[] )
         return -1;
     }
 
+    bool aOK = false;
+    auto debugLevel = parser.value( debugOption ).toInt( &aOK );
+    if ( !aOK )
+    {
+        std::cerr << "Could not process debug level '" << parser.value( debugOption ).toStdString() << "' must be an integer\n";
+        return -1;
+    }
+
+    auto port = parser.value( debugPortOption ).toInt( &aOK );
+    if ( !aOK || ( ( port < 0 ) || ( port > 65535 ) ) )
+    {
+        std::cerr << "Could not process debug port '" << parser.value( debugPortOption ).toStdString() << "' must be an integer between 0 and 65535\n";
+        return -1;
+    }
+    setupDebug( debugLevel, port );
+
     auto inputTeX = parser.value( inputTeXOption );
     int retVal = -1;
     std::cout << "Rendering TeX input: '" << inputTeX.toStdString() << "'" << "\n";
@@ -66,13 +137,14 @@ int main( int argc, char *argv[] )
         inputTeX,   //
         [ &engine, outputFile, &retVal ]( const std::optional< QByteArray > &svg )   //
         {
-            std::cout << "Finished rendering\n";
             if ( !svg.has_value() )
             {
-                std::cerr << "ERROR: Problem rendering Tex: " + engine.errorMessage().toStdString() << "\n";
+                std::cerr << "ERROR: Problem rendering TeX: see previous errors.\n";
                 retVal = -1;
                 return;
             }
+
+            std::cout << "Finished rendering\n";
 
             QFile outfile( outputFile );
             if ( !outfile.open( QFile::WriteOnly | QFile::Truncate | QFile::Text ) )
@@ -85,7 +157,12 @@ int main( int argc, char *argv[] )
             outfile.write( svg.value() );
             std::cout << "Finished writing file '" << outputFile.toStdString() << "'." << "\n";
             retVal = 0;
-        } );
+        },
+        [ = ]( const QString &msg )
+        {
+            std::cerr << "ERROR: " << msg.toStdString() << "\n";
+        }   //
+    );
 
     return retVal;
 }
